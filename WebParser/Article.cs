0f139c88f39fc3807af5ea6146d9596e0af27579
NightOOwl <titov.arsenij@yandex.ru>
2024-02-08 17:10:22 +0300

    Добавьте файлы проекта.

---
+++ WebParser/Article.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebParser
+{
+    public class Article
+    {
+        public string Url { get; set; }
+        public string PublishDate { get; set; }
+        public string Difficulty {  get; set; }
+        public string TimeToRead { get; set; }
+        public string Title { get; set; }
+        public string[] Body { get; set; }
+        public Article( string difficulty, string timeToRead, string title, string[] body, string url , string publishDate)
+        {
+            Difficulty = difficulty;
+            TimeToRead = timeToRead;
+            Title = title;
+            Body = body;
+            Url = url;
+            PublishDate = publishDate;
+        }
+        
+    }
+}
---
+++ WebParser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Channels;
+
+namespace WebParser
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await Console.Out.WriteLineAsync("Введите url статьи с Хабра для парсинга: ");
+            string url = Console.ReadLine();
+            var article = Parser.CreateArticle(await Parser.GetHTML(url),url);            
+            await ArticleWriter.WriteArticleAsync(article);
+        }      
+    }
+}
